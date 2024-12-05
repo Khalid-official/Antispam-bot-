b7914eeeb191c6c246f2df0a6f4311cee53ec4c6
KHALID OFFICIAL <113862545+Khalid-official@users.noreply.github.com>
2024-12-06 00:11:44 +0300

    Create _antivirus.js

---
+++ plugins/_antivirus.js
@@ -0,0 +1,18 @@
+const handler = (m) => m;
+
+handler.all = async function (m, { isBotAdmin }) {
+  // Check if the message is in a personal chat
+  if (!m.isGroup && m.messageStubType === 68) {
+    const log = {
+      key: m.key,
+      content: m.msg,
+      sender: m.sender,
+    };
+    // Perform the block operation instead of remove
+    await this.modifyChat(m.chat, 'block', {
+      includeStarred: false,
+    }).catch(console.log);
+  }
+};
+
+export default handler;
